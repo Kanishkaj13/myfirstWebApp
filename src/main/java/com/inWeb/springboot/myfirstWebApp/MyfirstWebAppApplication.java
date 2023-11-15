ceebe7c9b54bdd8ed79f611c853f86292a1cc9c9
Kanishkaj13 <kanishjeyaraja@gmail.com>
2023-11-15 08:33:36 +0530

    Initial commit

---
+++ src/main/java/com/inWeb/springboot/myfirstWebApp/MyfirstWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.inWeb.springboot.myfirstWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstWebAppApplication.class, args);
+	}
+
+}
